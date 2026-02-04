12edb942840711065c97cadf9093a5f5ef5cabde
ぱちお <patioglass@pachionoMac-mini.local>
2026-02-04 13:36:26 +0900

    Router設定修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useSetAtom, useAtomValue } from 'jotai';
+import { Routes, Route, useLocation, HashRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Works } from './pages/Works';
 {/*import { Bio } from './pages/Bio';*/}
 import { Contact } from './pages/Contact';
 import { Header } from './components/Header';
-import { worksAtom, fetchWorks } from './store/atoms';
 
 // ルート変更時にページトップへスクロール
 const ScrollToTop = () => {
@@ -19,36 +17,22 @@ const ScrollToTop = () => {
   return null;
 };
 
-// アプリ起動時にデータを取得する初期化コンポーネント
-const DataInitializer = ({ children }: { children: React.ReactNode }) => {
-  const setWorks = useSetAtom(worksAtom);
-  const works = useAtomValue(worksAtom);
 
-  useEffect(() => {
-    if (works.length === 0) {
-      fetchWorks().then(data => setWorks(data)).catch(console.error);
-    }
-  }, [works.length, setWorks]);
-
-  return <>{children}</>;
-};
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename="/">
       <ScrollToTop />
-      <DataInitializer>
-        <div className="min-h-screen bg-gray-50">
-          <Header />
+      <div className="min-h-screen bg-gray-50">
+        <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/works" element={<Works />} />
             {/*<Route path="/bio" element={<Bio />} />*/}
             <Route path="/contact" element={<Contact />} />
           </Routes>
-        </div>
-      </DataInitializer>
-    </Router>
+      </div>
+    </HashRouter>
   );
 }
 
